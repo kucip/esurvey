a5d81bda3e5277736986801a08413cc4df6951c6
Wahyu Budiman <wahyu.budiman@impact360design.com>
2024-06-11 13:39:04 +0800

    proteksi

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -24,7 +24,7 @@
     }
 
 </style>
-        
+
         <div class="row"> 
           <div class="col-xxl-12">
             <div class="section-box">
@@ -34,7 +34,6 @@
                     <h5>Biodata Responden</h5>
                   </div>
                   <div class="panel-body">
-
                     <div class="row"> 
                       <div class="col-lg-6 col-md-6">
                         <div class="form-group mb-30"> 
@@ -44,8 +43,8 @@
                       </div>
                       <div class="col-lg-6 col-md-6">
                         <div class="form-group mb-30">
-                          <label class="font-sm color-text-mutted mb-10" id="jkelamin">JENIS KELAMIN</label>
-                          <select class="form-control">
+                          <label class="font-sm color-text-mutted mb-10" >JENIS KELAMIN</label>
+                          <select class="form-control" id="jkelamin">
                               <option value="">-PILIH-</option>
                               <option value="1">Laki-Laki</option>
                               <option value="2">Perempuan</option>
@@ -110,14 +109,19 @@
                         </div>
                       </div>
 
+                      <div class="col-lg-12" style="margin-top: 20px;padding-right: 20px;"> 
+                        <div class="alert alert-danger" id="notif" role="alert" style="display: none;"></div>
+                      </div>
+                      
+                      <div class="" id="movescreen"></div>
                       <div class="col-lg-12" style="margin-top: 20px;"> 
                         <div class="form-group">
                           <button class="btn btn-default center" onclick="lanjut()">Lanjut Survey</button>
+                          <a href="#movescreen" id="btmovescreen">&nbsp;</a>
                         </div>
                       </div>
 
                     </div>
-
                   </div>
                 </div>
               </div>
@@ -133,27 +137,31 @@
                       $i=1;
                     @endphp
                     @foreach($pertanyaan as $val)
-                      <div class="form-group col-lg-6">
+                      <div class="form-group col-lg-6" style="padding-right: 10px;">
                         <div class="font-sq">{{$i}}</div>
-                        <div class="font-sm">{{$val->surPertanyaan}}</div>                          
-                        <div class="option"><input type="radio" id="option{{$val->surId}}_1" name="q_{{$val->surId}}" value="1"><label class="radio-label" for="option{{$val->surId}}_1">{{$val->surOpt1}}</label><br></div>
-                        <div class="option"><input type="radio" id="option{{$val->surId}}_2" name="q_{{$val->surId}}" value="2"><label class="radio-label" for="option{{$val->surId}}_2">{{$val->surOpt2}}</label><br></div>
-                        <div class="option"><input type="radio" id="option{{$val->surId}}_3" name="q_{{$val->surId}}" value="3"><label class="radio-label" for="option{{$val->surId}}_3">{{$val->surOpt3}}</label><br></div>
-                        <div class="option"><input type="radio" id="option{{$val->surId}}_4" name="q_{{$val->surId}}" value="4"><label class="radio-label" for="option{{$val->surId}}_4">{{$val->surOpt4}}</label><br></div>
-                        <div class="option"><input type="radio" id="option{{$val->surId}}_5" name="q_{{$val->surId}}" value="5"><label class="radio-label" for="option{{$val->surId}}_5">{{$val->surOpt5}}</label><br></div>
+                        <div class="font-sm">{{$val->surPertanyaan}}<input type="hidden" name="idtanya_{{$i}}" id="idtanya_{{$i}}" value="{{$val->surId}}"></div>                          
+                        <div class="option"><input type="radio" id="option{{$i}}_1" name="q_{{$i}}" value="1"><label class="radio-label" for="option{{$i}}_1">{{$val->surOpt1}}</label><br></div>
+                        <div class="option"><input type="radio" id="option{{$i}}_2" name="q_{{$i}}" value="2"><label class="radio-label" for="option{{$i}}_2">{{$val->surOpt2}}</label><br></div>
+                        <div class="option"><input type="radio" id="option{{$i}}_3" name="q_{{$i}}" value="3"><label class="radio-label" for="option{{$i}}_3">{{$val->surOpt3}}</label><br></div>
+                        <div class="option"><input type="radio" id="option{{$i}}_4" name="q_{{$i}}" value="4"><label class="radio-label" for="option{{$i}}_4">{{$val->surOpt4}}</label><br></div>
+                        <div class="option"><input type="radio" id="option{{$i}}_5" name="q_{{$i}}" value="5"><label class="radio-label" for="option{{$i}}_5">{{$val->surOpt5}}</label><br></div>
                       </div>
                       @php
                         $i++;
                       @endphp
                     @endforeach
 
-                    <div class="form-group col-lg-12">
+                    <div class="form-group col-lg-12" style="padding-right: 20px;">
                       <label class="font-sm">KRITIK DAN SARAN</label>
-                      <textarea class="form-control" rows="4" placeholder="Isilah Kritik dan Saran"></textarea>
+                      <textarea class="form-control" rows="4" placeholder="Isilah Kritik dan Saran" id="saran"></textarea>
                     </div>
 
+                      <div class="col-lg-12 col-lg-12" style="margin-top: 20px;padding-right: 20px;"> 
+                        <div class="alert alert-danger" id="warningjawaban" role="alert" style="display: none;"></div>
+                      </div>
+
                       <div class="form-group col-lg-12" style="margin-top:30px;">
-                        <button class="btn btn-default center">Simpan Survey</button>
+                        <button class="btn btn-default center" onclick="save()">Simpan Survey</button>
                       </div>
                       &nbsp;
                   </div>
@@ -164,6 +172,7 @@
         </div>
 
 <script type="text/javascript">
+
     function lanjut(){
         var nama=document.getElementById("nama").value;
         var jkelamin=document.getElementById("jkelamin").value;
@@ -174,8 +183,108 @@
         var layanan=document.getElementById("layanan").value;
         var unit=document.getElementById("unitkerja").value;
 
+        var txt='';
+        document.getElementById("notif").style="display:none";
+        $('#notif').html(txt);
+        var status=false;
+        if(nama==''){
+            txt +='- Isian < nama > masih kosong !!!<br>';
+            status = true;
+        }
 
+        if(jkelamin==''){
+            txt +='- Isian < jenis kelamin > masih kosong !!!<br>';
+            status = true;
+        }
+
+        if(umur==''){
+            txt +='- Isian < umur > masih kosong !!!<br>';
+            status = true;
+        }
+
+        if(pendidikan==''){
+            txt +='- Isian < pendidikan > masih kosong !!!<br>';
+            status = true;
+        }
+
+        if(layanan==''){
+            txt +='- Isian < layanan > masih kosong !!!<br>';
+            status = true;
+        }
+
+        if(unit==''){
+            txt +='- Isian < unit > masih kosong !!!<br>';
+            status = true;
+        }
+
+        if(status){
+           document.getElementById("notif").style="display:block";
+           $('#notif').html(txt);
+           return;
+        }
+
+        document.getElementById("btmovescreen").click();
         document.getElementById("boxpertanyaan").style="display:block";
     }
+
+    function save(){
+
+        var nama=document.getElementById("nama").value;
+        var jkelamin=document.getElementById("jkelamin").value;
+        var nohp=document.getElementById("nohp").value;
+        var alamat=document.getElementById("alamat").value;
+        var umur=document.getElementById("umur").value;
+        var pendidikan=document.getElementById("pendidikan").value;
+        var layanan=document.getElementById("layanan").value;
+        var unit=document.getElementById("unitkerja").value;
+        var saran=document.getElementById("saran").value;
+
+        var posData={};
+            posData.dataNama = nama;
+            posData.dataKelamin = jkelamin;
+            posData.dataHp = nohp;
+            posData.dataAlamat = alamat;
+            posData.dataUmur = umur;
+            posData.dataPendidikan = pendidikan;
+            posData.dataLayanan = layanan;
+            posData.dataUnit = unit;
+            posData.dataSaran = saran;
+
+        var textWarning='';
+        document.getElementById("warningjawaban").style="display:none";
+        $('#warningjawaban').html(textWarning);
+        var status=false;
+        for(var i=1;i<=10;i++){
+            var text1='posData.dataTanya'+i+'='+document.getElementById('idtanya_'+i).value || '';
+            eval(text1);
+            var radioEL=document.getElementsByName('q_'+i);
+            var radValue=0;
+            for(j=0;j<5;j++){
+               if(radioEL[j].checked){
+                  radValue=j+1;
+               }
+            }
+            if(radValue==0){
+               textWarning +='- Pertanyaan nomor #'+i+' belum dijawab !!!<br>';
+               status=true;
+            }
+
+            var text2='posData.dataJawab'+i+'='+radValue;
+            eval(text2);
+        }
+        
+        if(saran.trim()==""){
+           textWarning +='- Kritik dan Saran belum diisi !!!<br>';
+           status=true;          
+        }
+
+        if(status){
+           document.getElementById("warningjawaban").style="display:block";
+           $('#warningjawaban').html(textWarning);
+           return;
+        }
+
+    }
+
 </script>
 <x-web_templete_bottom />
